6d7b349a65841d3b86d787df8b355e1100721406
AmarSivas <amarsivas.b@gmail.com>
2021-10-01 18:13:03 +0530

    commented errors
    commented errors

--- src/main/java/com/docsconsole/tutorials/overridingwithcovariantreturntype/Car.java
+++ src/main/java/com/docsconsole/tutorials/overridingwithcovariantreturntype/Car.java
@@ -3,9 +3,9 @@ package com.docsconsole.tutorials.overridingwithcovariantreturntype;
 
 public class Car extends Vehicle {
 
-    public String run() {
+    /*public String run() {
         System.out.println("Car is running");
         return "Test";
-    }
+    }*/
 
 }
